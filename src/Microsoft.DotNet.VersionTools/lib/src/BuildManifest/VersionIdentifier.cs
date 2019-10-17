0576289aeedb8a6b32ca30f097235234cef4ab2d
mmitche <mmitche@microsoft.com>
2019-10-17 15:54:54 -0700

    WIP

--- src/Microsoft.DotNet.VersionTools/lib/src/BuildManifest/VersionIdentifier.cs
+++ src/Microsoft.DotNet.VersionTools/lib/src/BuildManifest/VersionIdentifier.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using Microsoft.DotNet.VersionTools.Util;
+using NuGet.ContentModel;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -24,20 +25,42 @@ namespace Microsoft.DotNet.VersionTools.BuildManifest
                 "rc"
             };
 
+        private const string _finalSuffix = "final";
+
         private static readonly char[] _delimiters = new char[] { '.', '-' };
 
         /// <summary>
-        /// Identify the version number of an asset.
+        /// Identify the version of an asset.
         /// 
         /// Assets can come in two forms:
         /// - Blobs that include the full path
         /// - Packages that do not include any path elements.
         /// 
-        /// This method expects that a single path element (e.g. the file name or
-        /// a blob directory that contains a version number)
+        /// This method starts at the last segment of the path and works backward to find a version number.
+        /// </summary>
+        /// <param name="assetName">Asset name</param>
+        /// <returns>Version, or null if none is found.</returns>
+        public static string GetVersion(string assetName)
+        {
+            string[] pathSegments = assetName.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+            string potentialVersion = null;
+            for (int i = pathSegments.Length - 1; i >= 0; i--)
+            {
+                potentialVersion = GetVersion(pathSegments[i]);
+                if (potentialVersion != null)
+                {
+                    break;
+                }
+            }
+
+            return potentialVersion;
+        }
+
+        /// <summary>
+        /// Identify the version number of an asset segment.
         /// </summary>
         /// <param name="assetName">Asset Name</param>
-        /// <returns>Version number, or nothing if none was found</returns>
+        /// <returns>Version number, or null if none was found</returns>
         /// <remarks>
         /// This is particularly error prone. To constrain the problem, we apply the following assumptions
         /// which are valid for .NET Core:
@@ -46,210 +69,116 @@ namespace Microsoft.DotNet.VersionTools.BuildManifest
         /// - We use additional numbers in our version numbers after the initial major.minor.patch, but any non-numeric element will end the version string
         /// - The delimiters we use in versions and file names are just . and -.
         /// </remarks>
-        public static string GetVersion2(string assetName)
+        private static string GetVersionForSingleSegment(string assetName)
         {
-            if (assetName.IndexOf('/') != -1)
-            {
-                throw new ArgumentException("Expected single path element");
-            }
 
             // Find the start of the version number by finding the major.minor.patch.
-            // Scan the string forward looking for a digit preceded
-            // Begin the scan for major.minor.patch.
-        }
+            // Scan the string forward looking for a digit preceded by one of the delimiters,
+            // then look for a minor.patch, completing the major.minor.patch.  Continue to do so until we get
+            // to something that is NOT major.minor.patch (this is necessary because we sometimes see things like:
+            // VS.Redist.Common.NetCore.Templates.x86.2.2.3.0.101-servicing-014320.nupkg
 
-        public static string GetVersion(string assetName)
-        {
-            string pathVersion = null;
-
-            if (assetName.Contains('/'))
+            int currentIndex = 0;
+            // Stack of major.minor.patch.
+            Stack<int> majorMinorPatchStack = new Stack<int>(3);
+            string majorMinorPatch = null;
+            StringBuilder versionSuffix = new StringBuilder();
+            char prevDelimiterCharacter = char.MinValue;
+            char nextDelimiterCharacter = char.MinValue;
+            while (true)
             {
-                string[] pathSegments = assetName.Split('/');
-                pathVersion = CheckIfVersionInPath(pathSegments);
-                assetName = pathSegments[pathSegments.Length - 1];
-            }
-
-            string[] segments = assetName.Split('.');
-            StringBuilder sb = new StringBuilder();
-            int versionStart = 0;
-            int versionEnd = 0;
-
-            for (int i = 1; i < segments.Length; i++)
-            {
-                if (IsMajorAndMinor(segments[i - 1], segments[i]))
+                string nextSegment;
+                prevDelimiterCharacter = nextDelimiterCharacter;
+                int nextDelimiterIndex = assetName.IndexOfAny(_delimiters, currentIndex);
+                if (nextDelimiterIndex != -1)
                 {
-                    versionStart = i - 1;
-                    versionEnd = i;
-                    i++;
+                    nextDelimiterCharacter = assetName[nextDelimiterIndex];
+                    nextSegment = assetName.Substring(currentIndex, nextDelimiterIndex - currentIndex);
+                }
+                else
+                {
+                    nextSegment = assetName.Substring(currentIndex, assetName.Length - currentIndex);
+                }
 
-                    // Once we have a major and minor we continue to check all the segments and if any have digits in it versionEnd
-                    // is updated. So far, produced assets don't have an extension with digits in it, if that changes we'd need to update
-                    // this logic
-                    while (i < segments.Length)
+                // If we have not yet found the major/minor/patch, then there are four cases:
+                // - There have been no potential major/minor/patch numbers found and the current segment is a number. Push onto the majorMinorPatch stack
+                //   and continue.
+                // - There has been at least one number found, but less than 3, and the current segment not a number or not preceded by '.'. In this case,
+                //   we should clear out the stack and continue the search.
+                // - There have been at least 2 numbers found and the current segment is a number and preceded by '.'. Push onto the majorMinorPatch stack and continue
+                // - There have been at least 3 numbers found and the current segment is not a number or not preceded by -. In this case, we can call this the major minor
+                //   patch number and no longer need to continue searching
+                if (majorMinorPatch == null)
+                {
+                    bool isNumber = int.TryParse(nextSegment, out int potentialVersionSegment);
+                    if ((majorMinorPatchStack.Count == 0 && isNumber) ||
+                        (majorMinorPatchStack.Count > 0 && prevDelimiterCharacter == '.' && isNumber))
                     {
-                        if (IsValidSegment(segments[i]))
-                        {
-                            versionEnd = i;
-                        }
-
-                        i++;
+                        majorMinorPatchStack.Push(potentialVersionSegment);
                     }
+                    // Check for partial major.minor.patch cases, like: 2.2.bar or 2.2-100.bleh
+                    else if (majorMinorPatchStack.Count > 0 && majorMinorPatchStack.Count < 3 &&
+                             (prevDelimiterCharacter != '.' || !isNumber))
+                    {
+                        majorMinorPatchStack.Clear();
+                    }
+                    
+                    // Determine whether we are done with major.minor.patch after this update.
+                    if (majorMinorPatchStack.Count >= 3 && (prevDelimiterCharacter != '.' || !isNumber || nextDelimiterIndex == -1))
+                    {
+                        // Done with major.minor.patch, found. Pop the top 3 elements off the stack.
+                        int patch = majorMinorPatchStack.Pop();
+                        int minor = majorMinorPatchStack.Pop();
+                        int major = majorMinorPatchStack.Pop();
+                        majorMinorPatch = $"{major}.{minor}.{patch}";
+                    }
+                }
+                
+                // Don't use else, so that we don't miss segments
+                // in case we are just deciding that we've finished major minor patch.
+                if (majorMinorPatch != null)
+                {
+                    // Now look at the next segment. If it looks like it could be part of a version. If it can't, then
+                    // we're done.
+                    // Cases where we should break out and be done:
+                    // - We have an empty pre-release label and the delimiter is not '-'.
+                    // - We have an empty pre-release label and the next segment does not start with a know tag.
+                    // - We have a non-empty pre-release label and the current segment is not a number.
+                    if (versionSuffix.Length == 0 &&
+                        (prevDelimiterCharacter != '-' || !_knownTags.Any(tag => nextSegment.StartsWith(tag, StringComparison.OrdinalIgnoreCase))))
+                    {
+                        return majorMinorPatch;
+                    }
+                    else if (versionSuffix.Length != 0 && !int.TryParse(nextSegment, out int potentialVersionSegment) && nextSegment != _finalSuffix)
+                    {
+                        return $"{majorMinorPatch}{versionSuffix.ToString()}";
+                    }
+                    else
+                    {
+                        // Append the delimiter character and then the current segment
+                        versionSuffix.Append(prevDelimiterCharacter);
+                        versionSuffix.Append(nextSegment);
+                    }
+                }
+
+                if (nextDelimiterIndex != -1)
+                {
+                    currentIndex = nextDelimiterIndex + 1;
+                }
+                else
+                {
+                    break;
                 }
             }
 
-            if (versionStart == versionEnd)
-            {
-                return pathVersion;
-            }
-
-            // Append major which might cointain fragments of the name so we need to only get the numeric piece out of that
-            string major = GetMajor(segments[versionStart++]);
-            sb.Append($"{major}.");
-
-            while (versionStart < versionEnd)
-            {
-                sb.Append($"{segments[versionStart++]}.");
-            }
-
-            sb.Append($"{segments[versionEnd]}");
-
-            string version = sb.ToString();
-
-            if (!string.IsNullOrEmpty(pathVersion) && string.IsNullOrEmpty(version))
+            if (string.IsNullOrEmpty(majorMinorPatch))
             {
                 return null;
             }
 
-            if (!string.IsNullOrEmpty(pathVersion) && _knownTags.Any(t => version.Contains(t)) && _knownTags.Any(k => pathVersion.Contains(k)))
-            {
-                return version.Length < pathVersion.Length ? version : pathVersion;
-            }
-
-            if (string.IsNullOrEmpty(pathVersion) || _knownTags.Any(t => version.Contains(t)))
-            {
-                return version;
-            }
-
-            return pathVersion;
+            return $"{majorMinorPatch}{versionSuffix.ToString()}";
         }
 
-        private static string CheckIfVersionInPath(string[] pathSegments)
-        {
-            foreach (string pathSegment in pathSegments)
-            {
-                string version = GetVersion(pathSegment);
 
-                if (!string.IsNullOrEmpty(version))
-                {
-                    return version;
-                }
-            }
-
-            return null;
-        }
-
-        private static bool IsMajorAndMinor(string major, string minor)
-        {
-            return major.Any(char.IsDigit) && int.TryParse(minor, out int min);
-        }
-
-        private static string GetMajor(string versionSegment)
-        {
-            if (int.TryParse(versionSegment, out int v))
-            {
-                return versionSegment;
-            }
-
-            int index = versionSegment.Length - 1;
-            List<char> version = new List<char>();
-
-            while (index > 0 && char.IsDigit(versionSegment[index]))
-            {
-                version.Insert(0, versionSegment[index--]);
-            }
-
-            return new string(version.ToArray());
-        }
-
-        private static bool IsValidSegment(string versionSegment)
-        {
-            return versionSegment.Any(char.IsDigit) || _knownTags.Any(t => versionSegment.Contains(t));
-        }
-
-        /// <summary>
-        /// Remove any version numbers from an asset name
-        /// </summary>
-        /// <param name="assetName">Asset name</param>
-        /// <remarks>
-        /// This method will attempt to strip away any version info from the asset id.
-        /// Stripping away all the version info is not totally trivial.
-        /// Multiple version numbers may exist within the blob path, and these version may not
-        /// be identical. Example:
-        /// - Runtime/3.0.1-servicing-19511-02/dotnet-host-3.0.1-x64.rpm
-        /// 
-        /// This is a bit problematic since it means that an attempt to use the version manager to determine
-        /// the version will result in one of the two versions. Replacing the returned version throughout the
-        /// string may result in an incorrect blob path:
-        /// - Runtime/-servicing-19511-02/dotnet-host-3.0.1-x64.rpm
-        /// 
-        /// The good news is that there should never be more than two versions that should need to be stripped
-        /// from any path: the stable version number and the non-stable (suffixed) version number. Most of the time there
-        /// will be only one (a suffixed-version)
-        /// 
-        /// One algorithm that can work to end up with the right version is this:
-        /// 1. Extract the file name from the final path.
-        /// 2. Pass both the full path and the file name to the version identifier.
-        /// 3. If the versions returned are both non-null, first remove the longest of the two strings from the
-        ///    full path, then the shorter.
-        /// 
-        /// Note: This method does *not* remove any oddities introduced by removing the version elements. For instance,
-        /// If removing the version element ends up introducing a "//", it will remain. It's up to the caller to interpret these
-        /// appropriately and decide what to do.
-        /// </remarks>
-        public static string GetAssetWithoutVersions(string assetName)
-        {
-            string fullBlob = assetName;
-            // This could be either a file name or a blob path, so use the path
-            // utilities to get the file name.
-            string fileElement = Path.GetFileName(fullBlob);
-
-            List<string> versionsToRemove = new List<string>();
-            string versionFromFullBlob = VersionIdentifier.GetVersion(fullBlob);
-
-            if (!string.IsNullOrEmpty(versionFromFullBlob))
-            {
-                versionsToRemove.Add(versionFromFullBlob);
-            }
-
-            // If the file element is not the same as the full blob (if there were path elements)
-            if (fileElement != fullBlob)
-            {
-                string versionFromFileElement = VersionIdentifier.GetVersion(fileElement);
-
-                if (!string.IsNullOrEmpty(versionFromFileElement))
-                {
-                    versionsToRemove.Add(versionFromFileElement);
-                }
-            }
-
-            // If any other types of versions appear in the future, add them to the list here.
-            string fullBlobWithoutVersions = fullBlob;
-            if (versionsToRemove.Count > 0)
-            {
-                // Things to remove!
-                versionsToRemove.Sort();
-
-                // The key here is that we remove elements that are substrings of other later *after*,
-                // so iterate backwards through the list.
-                for (int i = versionsToRemove.Count - 1; i >= 0; i--)
-                {
-                    fullBlobWithoutVersions = fullBlobWithoutVersions.Replace(versionsToRemove[i], "");
-                }
-            }
-            
-            // Now remove any double path elements, as well as any cases where 
-
-            return fullBlobWithoutVersions;
-        }
     }
 }
--- src/Microsoft.DotNet.VersionTools/tests/BuildManifest/VersionIdentiferTests.cs
+++ src/Microsoft.DotNet.VersionTools/tests/BuildManifest/VersionIdentiferTests.cs
@@ -6,11 +6,43 @@ using System.Collections.Generic;
 using System.IO;
 using Xunit;
 using Microsoft.DotNet.VersionTools.BuildManifest;
+using NuGet.ContentModel;
 
 namespace Microsoft.DotNet.VersionTools.Tests.BuildManifest
 {
     public class VersionTests
     {
+        // Simple set of versions from inline data to call out simple failure cases.
+        [Theory]
+        [InlineData("1.0.0", "1.0.0")]
+        [InlineData("10.0.1", "10.0.1")]
+        [InlineData("10.0.1-", "10.0.1")]
+        [InlineData("10.0.1-beta.final", "10.0.1-beta.final")]
+        [InlineData("10.0.1-preview1.12345.1", "10.0.1-preview1.12345.1")]
+        [InlineData("FooPackage.1.0.0", "1.0.0")]
+        [InlineData("FooPackage.10.0.1", "10.0.1")]
+        [InlineData("FooPackage.10.0.1-beta.final", "10.0.1-beta.final")]
+        [InlineData("FooPackage.10.0.1-preview1.12345.1", "10.0.1-preview1.12345.1")]
+        [InlineData("What.FooPackage.1.0.0", "1.0.0")]
+        [InlineData("What.2.2.FooPackage.10.0.1", "10.0.1")]
+        [InlineData("What.FooPackage.10.0.1-beta.final", "10.0.1-beta.final")]
+        [InlineData("What.1.FooPackage.10.0.1-preview1.12345.1", "10.0.1-preview1.12345.1")]
+        [InlineData("What-Is-A.FooPackage.1.0.0", "1.0.0")]
+        [InlineData("What-Is-A.FooPackage.10.0.1", "10.0.1")]
+        [InlineData("What-Is-A.FooPackage.10.0.1-beta.final", "10.0.1-beta.final")]
+        [InlineData("What-Is-A.FooPackage.10.0.1-preview1.12345.1", "10.0.1-preview1.12345.1")]
+        [InlineData("What-Is-A.FooPackage.2.2.1.0.0", "1.0.0")]
+        [InlineData("What-Is-A.FooPackage.2.2.10.0.1", "10.0.1")]
+        [InlineData("What-Is-A.FooPackage.2.2.10.0.1-beta.final", "10.0.1-beta.final")]
+        [InlineData("What-Is-A.FooPackage.2.2.10.0.1-preview1.12345.1", "10.0.1-preview1.12345.1")]
+        [InlineData("What-Is-A.FooPackage", null)]
+        [InlineData("What-Is-A.FooPackage-2.2-64", null)]
+        [InlineData("What-Is-A.FooPackage-2.2.nupkg", null)]
+        public void ValidateSimpleVersions(string assetName, string version)
+        {
+            Assert.Equal(version, VersionIdentifier.GetVersion(assetName));
+        }
+
         [Fact]
         public void ValidateVersions()
         {
@@ -19,14 +51,13 @@ namespace Microsoft.DotNet.VersionTools.Tests.BuildManifest
             foreach (VersionIdentifierTestAsset testAsset in testAssets)
             {
                 Assert.Equal(testAsset.ExpectedVersion, VersionIdentifier.GetVersion(testAsset.Name));
-                Assert.Equal(testAsset.NameWithoutVersions, VersionIdentifier.GetAssetWithoutVersions(testAsset.Name));
             }
         }
 
         private List<VersionIdentifierTestAsset> GetTestAssets()
         {
             List<VersionIdentifierTestAsset> testAssets = new List<VersionIdentifierTestAsset>();
-            string[] assets = File.ReadAllLines("BuildManifest/VersionIdentifierTestsAssets-modified.csv");
+            string[] assets = File.ReadAllLines("BuildManifest/VersionIdentifierTestsAssets.csv");
 
             foreach (string input in assets)
             {
@@ -35,7 +66,8 @@ namespace Microsoft.DotNet.VersionTools.Tests.BuildManifest
                     string[] values = input.Split(',');
                     string name = values[0];
                     string expectedVersion = string.IsNullOrEmpty(values[1]) ? null : values[1];
-                    string nameWithoutVersions = string.IsNullOrEmpty(values[2]) ? null : values[2];
+                    // string nameWithoutVersions = string.IsNullOrEmpty(values[2]) ? null : values[2];
+                    string nameWithoutVersions = null;
                     testAssets.Add(new VersionIdentifierTestAsset(name, expectedVersion, nameWithoutVersions));
                 }
             }
