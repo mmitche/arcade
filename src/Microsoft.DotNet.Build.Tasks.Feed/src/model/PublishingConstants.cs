7f75923bc9b05df777186af86260547a8bed700d
Matt Mitchell <mmitche@microsoft.com>
2022-04-06 10:09:26 -0700

    Add VS 17.3 publishing

--- src/Microsoft.DotNet.Build.Tasks.Feed/src/model/PublishingConstants.cs
+++ src/Microsoft.DotNet.Build.Tasks.Feed/src/model/PublishingConstants.cs
@@ -999,6 +999,17 @@ namespace Microsoft.DotNet.Build.Tasks.Feed.Model
                 filenamesToExclude: FilenamesToExclude,
                 flatten: false),
 
+            // "VS 17.3",
+            new TargetChannelConfig(
+                2692,
+                false,
+                PublishingInfraVersion.All,
+                new List<string>(),
+                DotNetToolsFeeds,
+                PublicAndInternalSymbolTargets,
+                filenamesToExclude: FilenamesToExclude,
+                flatten: false),
+
             // ".NET Libraries",
             new TargetChannelConfig(
                 1648,
