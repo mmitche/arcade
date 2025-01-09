01a002c967b41dd42ed653af0feeff1b02849a88
Matt Mitchell (.NET) <mmitche@microsoft.com>
2025-01-09 15:11:16 -0800

    Set bundle id hopefully

--- src/Microsoft.DotNet.SignTool/src/SignTool.cs
+++ src/Microsoft.DotNet.SignTool/src/SignTool.cs
@@ -187,9 +187,10 @@ namespace Microsoft.DotNet.SignTool
                 }
                 AppendLine(builder, depth: 2, text: $@"<FilesToSign Include=""{Uri.EscapeDataString(filePath)}"">");
                 AppendLine(builder, depth: 3, text: $@"<Authenticode>{(notarize ? fileToSign.SignInfo.Notarization : fileToSign.SignInfo.Certificate)}</Authenticode>");
+                if (notarize)
                 if (fileToSign.SignInfo.ShouldStrongName && !fileToSign.SignInfo.ShouldLocallyStrongNameSign)
                 {
-                    AppendLine(builder, depth: 3, text: $@"<StrongName>{fileToSign.SignInfo.StrongName}</StrongName>");
+                    AppendLine(builder, depth: 3, text: $@"<MacAppName>com.microsoft.dotnet</MacAppName>");
                 }
                 AppendLine(builder, depth: 2, text: @"</FilesToSign>");
             }
