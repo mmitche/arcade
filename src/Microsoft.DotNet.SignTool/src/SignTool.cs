4d4be97193b3c014208183b6d97ccf4ceaeb6dea
Matt Mitchell (.NET) <mmitche@microsoft.com>
2025-01-10 12:29:08 -0800

    Attempt zipping again

--- src/Microsoft.DotNet.SignTool/src/SignTool.cs
+++ src/Microsoft.DotNet.SignTool/src/SignTool.cs
@@ -146,18 +146,17 @@ namespace Microsoft.DotNet.SignTool
                 return false;
             }
 
-            // Now unzip. Notarization does not expect zipped packages.
-            UnzipMacFiles(zippedPaths);
-
             // Then an additional notarization pass.
             var filesToNotarize = filesToSign.Where(f => !string.IsNullOrEmpty(f.SignInfo.NotarizationAppName));
             if (filesToNotarize.Any())
             {
-                // Now notarize. No need to unzip in between
                 var notarizeProjectPath = Path.Combine(dir, $"Round{round}-Notarize.proj");
-                File.WriteAllText(notarizeProjectPath, GenerateBuildFileContent(filesToNotarize, null, true));
+                File.WriteAllText(notarizeProjectPath, GenerateBuildFileContent(filesToNotarize, zippedPaths, true));
                 status = RunMSBuild(buildEngine, notarizeProjectPath, Path.Combine(_args.LogDir, $"NotarizationRound{round}.binlog"));
             }
+
+            // Now unzip. Notarization does not expect zipped packages.
+            UnzipMacFiles(zippedPaths);
 
             return status;
         }
